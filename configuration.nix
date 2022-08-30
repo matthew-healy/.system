e0c63e70461c0eaa6014dd8f3c157815b99f19ac
Matthew Healy <matthew.healy@tweag.io>
2022-08-30 14:41:52 +0200

    Add IOG cache

--- configuration.nix
+++ configuration.nix
@@ -41,6 +41,13 @@
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
+  nix.settings.trusted-public-keys = [
+    "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+  ];
+  nix.settings.substituters = [
+    "https://cache.iog.io"
+  ];
+
   users.users.matthew = {
     isNormalUser = true;
     description = "Matthew Healy";
