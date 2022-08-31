62ac142e5710677b3f12398bfa98048c704bce3f
Matthew Healy <matthew@liamhealy.xyz>
2022-08-31 23:20:36 +0200

    Add vscode settings

--- configuration.nix
+++ configuration.nix
@@ -57,7 +57,6 @@
       firefox
       gnome.gnome-sound-recorder
       slack
-      vscode
     ];
   };
 
@@ -156,6 +155,35 @@
 
         pull.rebase = true;
       };
+    };
+
+    programs.vscode = {
+      enable = true;
+
+      userSettings = {
+        "editor.fontFamily" = "'Fira Code', 'monospace', monospace";
+        "editor.fontLigatures" = true;
+        "editor.fontSize" = 16;
+        "editor.tabSize" = 2;
+        "explorer.confirmDragAndDrop" = false;
+        "haskell.manageHLS" = "PATH";
+        "telemetry.telemetryLevel" = "off";
+        "workbench.colorTheme" = "Pink Cat Boo";
+      };
+
+      extensions = with pkgs.vscode-extensions; [
+        arrterian.nix-env-selector
+        haskell.haskell
+        jnoortheen.nix-ide
+        justusadam.language-haskell
+      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "theme-pink-cat-boo";
+          publisher = "ftsamoyed";
+          version = "1.3.0";
+          sha256 = "FD7fim0sRWAADzDAbhV3dnYW3mxoSgVPLs5Wkg5r01k=";
+        }
+      ];
     };
   };
   
