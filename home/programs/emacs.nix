f9601e7d01e50e4f570e8a49406fae8ee357a511
Matthew Healy <matthew.healy@tweag.io>
2022-09-01 18:48:18 +0200

    Install emacs

---
+++ home/programs/emacs.nix
@@ -0,0 +1,18 @@
+{ ... }:
+{
+  emacs = {
+    enable = true;
+
+    extraConfig = ''
+      (setq backup-directory-alist
+        `(("." . ,(expand-file-name
+                    (concat user-emacs-directory "backups")))))
+
+      (setq backup-by-copying-when-linked t)
+
+      (add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+      (set-face-attribute 'default nil :height 140)
+    '';
+  };
+}
