eedb41c2ce0a7b2f4eb01395cf60dd0ac205c9d2
Matthew Healy <matthew.healy@tweag.io>
2022-09-19 15:50:44 +0200

    Update shell prompt when in a nix shell

--- home/programs/bash.nix
+++ home/programs/bash.nix
@@ -19,6 +19,10 @@
     '';
 
     bashrcExtra = ''
+      if [[ -n "$IN_NIX_SHELL" ]]; then
+        export PS1="\\033[01;38;5;182m[\u in nix-shell:\w]$\033[0m "
+      fi
+
       function mkd() {
         mkdir -p "$1" && cd "$_";
       };
