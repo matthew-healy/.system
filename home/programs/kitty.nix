fe254522bb3798c0c53b73958a2443edee5ec2b8
Matthew Healy <matthew.healy@tweag.io>
2022-10-12 20:33:58 +0200

    Install Kitty

---
+++ home/programs/kitty.nix
@@ -0,0 +1,14 @@
+{ ... }:
+{
+  kitty = {
+    enable = true;
+
+    settings = {
+      font_size = "13.0";
+      font_family = "FiraCode Nerd Font";
+      background_opacity = "0.98";
+    };
+
+    theme = "Tokyo Night Storm";
+  };
+}
