d323af19758229b285b738297df74ff445891f52
Matthew Healy <matthew.healy@tweag.io>
2022-10-27 16:26:48 +0200

    Re-structure home-manager modules

--- flake.nix
+++ flake.nix
@@ -40,7 +40,7 @@
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
             }
-            ./home/config.nix
+            ./home.nix
             ./fonts.nix
           ];
         };
