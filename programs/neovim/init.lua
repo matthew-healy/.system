925296475ceb19bc2befc6a362b532d7b92e875e
Matthew Healy <matthew.healy@tweag.io>
2022-10-27 21:17:42 +0200

    [nvim] Add rust-tools

--- programs/neovim/init.lua
+++ programs/neovim/init.lua
@@ -53,6 +53,30 @@ vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
 require('packer').startup(function(use)
   use 'LnL7/vim-nix'
   use 'airblade/vim-gitgutter'
+  use 'neovim/nvim-lspconfig'
+
+  use ({
+    'simrat39/rust-tools.nvim',
+    config = function()
+      local rt = require('rust-tools')
+      rt.setup({
+        server = {
+          on_attach = function(_, bufnr)
+            vim.keymap.set(
+              'n', '<C-space>',
+              rt.hover_actions.hover_actions,
+              { buffer = bufnr }
+            )
+            vim.keymap.set(
+              'n', '<C-a>',
+              rt.code_action_group.code_action_group,
+              { buffer = bufnr }
+            )
+          end
+        }
+      })
+    end
+  })
 
   use({
     'folke/tokyonight.nvim',
