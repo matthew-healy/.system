24b1deee3f5db6a83d93967515882babe7da6cab
Matthew Healy <matthew.healy@tweag.io>
2023-02-20 10:51:44 +0100

    Tmate config

--- programs/tmate.nix
+++ programs/tmate.nix
@@ -1,1 +1,19 @@
-{ ... }: { tmate.enable = true; }
+{ ... }: {
+  tmate = {
+    enable = true;
+
+    extraConfig = ''
+      # Fixes the delay in helix/vim when hitting esc
+      set -s escape-time 0
+
+      # Increase tmux message display duration
+      set -g display-time 4000
+
+      # Upgrade $TERM
+      set -g default-terminal "tmux-256color"
+
+      # Enable terminal focus events
+      set -g focus-events on
+    '';
+  };
+}
