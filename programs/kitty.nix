e759b6175cbf3b88348d79e5d39702fd35a2c008
Matthew Healy <matthew.healy@tweag.io>
2023-05-25 16:21:15 +0200

    Force kitty to use block cursor

--- programs/kitty.nix
+++ programs/kitty.nix
@@ -3,6 +3,8 @@
     enable = true;
 
     settings = {
+      cursor_shape = "block";
+      shell_integration = "no-cursor";
       font_size = "13.0";
       font_family = "FiraCode Nerd Font";
       background_opacity = "0.95";
