d9b6999486d0d11a7dd8539adcc9339a5086fe99
Matthew Healy <matthew@liamhealy.xyz>
2023-06-27 14:35:07 +0200

    Explicitly use wayland

--- profiles/workspace/gnome.nix
+++ profiles/workspace/gnome.nix
@@ -2,6 +2,7 @@
   services.xserver.enable = true;
 
   services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = true;
   services.xserver.desktopManager.gnome.enable = true;
 
   environment.gnome.excludePackages = [ pkgs.gnome-console ];
