5557a7bdddd41e85a385bc35bf98134c6fb6f9df
Matthew Healy <matthew@liamhealy.xyz>
2023-07-11 00:31:39 +0200

    Remove NIXOS_OZONE_WL, it stops 1password opening

--- profiles/applications/slack.nix
+++ profiles/applications/slack.nix
@@ -1,6 +1,3 @@
 { pkgs, ... }: {
-  # enables native wayland support for Slack (and maybe other stuff? idk)
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
   home-manager.users.matthew.home.packages = [ pkgs.slack ];
 }
