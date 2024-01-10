b970929422457b298cf7506825317ddfea3851de
Matthew Healy <matthew@liamhealy.xyz>
2024-01-10 18:48:36 +0100

    Fix Apple touchpad connection issue

--- profiles/workspace/bluetooth.nix
+++ profiles/workspace/bluetooth.nix
@@ -1,4 +1,13 @@
 {
-  hardware.bluetooth.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+
+    # Apple Trackpad won't work without this: https://github.com/bluez/bluez/issues/664
+    settings = {
+      General = {
+        ClassicBondedOnly = false;
+      };
+    };
+  };
   services.blueman.enable = true;
 }
