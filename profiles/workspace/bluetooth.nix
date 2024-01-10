8229490add6e29f5d51085a2905efee61e340ebc
Matthew Healy <matthew@liamhealy.xyz>
2024-01-10 21:19:53 +0100

    Add shell aliases for common bluetooth addresses

--- profiles/workspace/bluetooth.nix
+++ profiles/workspace/bluetooth.nix
@@ -10,4 +10,16 @@
     };
   };
   services.blueman.enable = true;
+
+  home-manager.users.matthew.programs.bash =
+    let
+      trackpad = "64:B0:A6:E6:CB:5B";
+      headphones = "90:9C:4A:DE:C1:61";
+    in
+    {
+      shellAliases = {
+        "bt-trackpad" = "echo \"${trackpad}\"";
+        "bt-headphones" = "echo \"${headphones}\"";
+      };
+    };
 }
