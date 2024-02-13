d7013c30c7bb34b9c9833763420fe51fcb0888a2
Matthew Healy <matthew@liamhealy.xyz>
2024-02-13 20:18:12 +0100

    Update nvidia setup in line with nixos.wiki

--- machines/home-pc/default.nix
+++ machines/home-pc/default.nix
@@ -6,8 +6,26 @@
   ];
 
   # TODO: move to profile?
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.opengl.enable = true;
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.latest;
-  hardware.nvidia.modesetting.enable = true;
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+
+    # Power management is experimental & was breaking sleep mode.
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+
+    open = false;
+
+    # Enables the settings menu (accessed via `nvidia-settings`).
+    nvidiaSettings = true;
+
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 }
