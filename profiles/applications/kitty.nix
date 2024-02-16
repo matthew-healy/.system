7e6b3cd0698059d53a49f90bf015aae74817c7d3
Matthew Healy <matthew@liamhealy.xyz>
2024-02-16 21:32:33 +0100

    Document kitty displayServer option

--- profiles/applications/kitty.nix
+++ profiles/applications/kitty.nix
@@ -1,5 +1,8 @@
 { pkgs, lib, config, ... }: {
   options = {
+    # Kitty 0.32+ doesn't get on well with Wayland + the current Nvidia drivers.
+    # A fix is supposedly coming in the 550 driver release, but until then it's
+    # good to have the option to easily switch a machine to x11.
     kitty.displayServer = lib.mkOption {
       type = lib.types.str;
       default = "auto";
