c483cff216da0b45534e72bc3741241e96eeb847
Matthew Healy <matthew@liamhealy.xyz>
2024-07-22 22:43:02 +0200

    Disable tap-to-click on touchpads

--- profiles/workspace/gnome.nix
+++ profiles/workspace/gnome.nix
@@ -8,4 +8,18 @@
   environment.gnome.excludePackages = [ pkgs.gnome-console ];
 
   home-manager.users.matthew.services.gnome-keyring.enable = true;
+
+  programs.dconf = {
+    enable = true;
+
+    profiles.matthew.databases = [
+      {
+        lockAll = true;
+        settings = {
+          "org/gnome/peripherals/touchpad".tap-to-click = false;
+          "org/gnome/desktop/interface".clock-show-weekday = true;
+        };
+      }
+    ];
+  };
 }
