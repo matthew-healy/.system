585b99b2619248b11bf3157a84bec4838a2a5f61
Matthew Healy <matthew@liamhealy.xyz>
2025-04-05 17:44:05 +0200

    Install signal

--- overlay.nix
+++ overlay.nix
@@ -1,3 +1,15 @@
 inputs: final: prev: {
   # declare any overlays here to make them available via pkgs everywhere else
+
+  # source: https://github.com/NixOS/nixpkgs/issues/222043 (from xavier's config)
+  signal-desktop = prev.signal-desktop.overrideAttrs (old: {
+    preFixup =
+      old.preFixup
+      + ''
+        gappsWrapperArgs+=(
+          --add-flags "--enable-features=UseOzonePlatform"
+          --add-flags "--ozone-platform=wayland"
+        )
+      '';
+  });
 }
