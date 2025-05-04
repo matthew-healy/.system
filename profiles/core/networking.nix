a3a8f50db39ebb9da2f3d1fd79ce3c733f71d3cc
Matthew Healy <matthew@liamhealy.xyz>
2025-05-04 15:42:16 +0200

    Fix Docker clashing with ICE wifi

--- profiles/core/networking.nix
+++ profiles/core/networking.nix
@@ -1,3 +1,18 @@
 { ... }: {
   networking.networkmanager.enable = true;
+
+  services.resolved = {
+    enable = true;
+  };
+
+  # fix address range clashing with WifiOnICE network on DB trains
+  # (thank you Lenny)
+  virtualisation.docker.daemon.settings = {
+    bip = "172.39.1.5/24";
+    fixed-cidr = "172.39.1.0/25";
+    default-address-pools = [{
+      base = "172.39.0.0/16";
+      size = 24;
+    }];
+  };
 }
