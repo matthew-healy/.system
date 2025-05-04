fdafda253b4d3bf2f62c777b879cf128e98f8745
Matthew Healy <matthew@liamhealy.xyz>
2025-05-04 15:42:38 +0200

    Add emoji picker

---
+++ profiles/core/desktop/emoji.nix
@@ -0,0 +1,9 @@
+{ pkgs, ... }: {
+  home-manager.users.matthew = {
+    home.packages = [ pkgs.bemoji pkgs.wl-clipboard pkgs.wtype ];
+
+    wayland.windowManager.hyprland.settings.bind = [
+      "$mod SHIFT, E, exec, bemoji -c"
+    ];
+  };
+}
