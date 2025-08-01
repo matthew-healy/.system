d0349be991b20f072e292ca2c97ce1b665939f96
Matthew Healy <matthew@liamhealy.xyz>
2025-08-01 09:32:22 +0200

    Set explicit user group

--- profiles/security/user.nix
+++ profiles/security/user.nix
@@ -1,7 +1,12 @@
 {
+  users.groups.matthew = {
+    gid = 1000;
+  };
+
   users.users.matthew = {
     isNormalUser = true;
     description = "matthew";
+    group = "matthew";
     extraGroups = [
       "audio"
       "docker"
