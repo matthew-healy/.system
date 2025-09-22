da109f9ac20b8a4700f4f2b3c32198ee46b0f25f
Matthew Healy <matthew@liamhealy.xyz>
2025-09-22 09:12:56 +0100

    Home office setup updates

--- profiles/core/desktop/monitors.nix
+++ profiles/core/desktop/monitors.nix
@@ -19,12 +19,14 @@
         in
         {
           "monitors-home" = stmts [
+            "${hd} workspace 5"
+            "${hd} moveworkspacetomonitor 5 ${monitors.laptop}"
             "for i in {1..4}"
             "do ${hd} moveworkspacetomonitor $i ${monitors.hdmi}"
             "done"
           ];
 
-          "monitors-work" = stmts [
+          "monitors-office" = stmts [
             "${hd} workspace 5"
             "${hd} moveworkspacetomonitor 5 ${monitors.laptop}"
             "for i in {1..4}"
@@ -49,12 +51,20 @@
             ];
           }
           {
-            profile.name = "hdmi-only";
+            profile.name = "laptop-and-hdmi";
             profile.outputs = [
               {
                 criteria = monitors.hdmi;
                 mode = "3840x2160@60Hz";
                 scale = 1.0;
+                position = "0,0";
+              }
+              {
+                criteria = monitors.laptop;
+                mode = "1920x1200@60Hz";
+                scale = 1.0;
+                status = "enable";
+                position = "960,2160";
               }
             ];
           }
