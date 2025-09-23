c2498b47afdf01551b8b5004175163c1ba978716
Matthew Healy <matthew@liamhealy.xyz>
2025-09-23 14:44:46 +0100

    Consistent cursor sizing

--- profiles/core/desktop/cursor.nix
+++ profiles/core/desktop/cursor.nix
@@ -1,10 +1,27 @@
-{ pkgs, ... }: {
-  home-manager.users.matthew.home.pointerCursor = {
-    enable = true;
-    package = pkgs.xcursor-pro;
-    name = "XCursor-Pro-Dark";
-    size = 24;
-    gtk.enable = true;
-    x11.enable = true;
+{ pkgs, ... }:
+{
+  home-manager.users.matthew = {
+    home.packages = with pkgs; [
+      hyprcursor
+      rose-pine-cursor
+      rose-pine-hyprcursor
+      nwg-look
+    ];
+
+    wayland.windowManager.hyprland.settings =
+      let
+        cursorSize = "24";
+      in
+      {
+        exec-once = [
+          "gsettings set org.gnome.desktop.interface cursor-theme BreezeX-RosePine-Linux"
+          "gsettings set org.gnome.desktop.interface cursor-size ${cursorSize}"
+        ];
+
+        env = [
+          "HYPRCURSOR_THEME,rose-pine-hyprcursor"
+          "HYPRCURSOR_SIZE,${cursorSize}"
+        ];
+      };
   };
 }
