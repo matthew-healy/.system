d017e6df37ad335b1e07982334e8a64c702c9892
Matthew Healy <matthew@liamhealy.xyz>
2025-10-07 09:25:16 +0100

    New monitor setup

--- profiles/core/desktop/monitors.nix
+++ profiles/core/desktop/monitors.nix
@@ -6,9 +6,7 @@
     let
       monitors = {
         laptop = "eDP-1";
-        hdmi = "HDMI-A-1";
         dp3 = "DP-3";
-        dp4 = "DP-4";
       };
     in
     {
@@ -18,15 +16,7 @@
           stmts = es: builtins.concatStringsSep "; " es;
         in
         {
-          "monitors-home" = stmts [
-            "${hd} workspace 5"
-            "${hd} moveworkspacetomonitor 5 ${monitors.laptop}"
-            "for i in {1..4}"
-            "do ${hd} moveworkspacetomonitor $i ${monitors.hdmi}"
-            "done"
-          ];
-
-          "monitors-office" = stmts [
+          "fix-workspace-layout" = stmts [
             "${hd} workspace 5"
             "${hd} moveworkspacetomonitor 5 ${monitors.laptop}"
             "for i in {1..4}"
@@ -51,35 +41,13 @@
             ];
           }
           {
-            profile.name = "laptop-and-hdmi";
-            profile.outputs = [
-              {
-                criteria = monitors.hdmi;
-                mode = "3840x2160@60Hz";
-                scale = 1.0;
-                position = "0,0";
-              }
-              {
-                criteria = monitors.laptop;
-                mode = "1920x1200@60Hz";
-                scale = 1.0;
-                status = "enable";
-                position = "960,2160";
-              }
-            ];
-          }
-          {
             profile.name = "laptop-and-usbc";
             profile.outputs = [
               {
                 criteria = monitors.dp3;
-                mode = "5120x2880@60Hz";
-                scale = 2.0;
+                mode = "3840x2160@120Hz";
+                scale = 1.0;
                 position = "0,0";
-              }
-              {
-                criteria = monitors.dp4;
-                status = "disable";
               }
               {
                 criteria = monitors.laptop;
